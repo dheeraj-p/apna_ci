26caad45f86bcf39059926fc0314e0d6f067fd2d
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-11-25 12:42:47 +0530

    Handle exceptions on invalid repository url

--- startci.sh
+++ startci.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
+set -e
 repository_url=$1
 repository_name=$(echo "$repository_url" | cut -d"/" -f5 | cut -d"." -f1)
 CI_REPOS_DIR=".ci_repositories"
-INTEGRATION_INTERVAL=600
+INTEGRATION_INTERVAL=5
 
 #Create ".ci_repositories" directory if it does not already exists
 if [ ! -d "$CI_REPOS_DIR" ]; then
@@ -14,9 +15,11 @@ cd "$CI_REPOS_DIR"
 #Clone repository if it is not already cloned
 if [ ! -d "$repository_name" ]; then
   git clone "$repository_url"
+
 fi
 cd "$repository_name"
 
+set +e
 #Start running test after every INTEGRATION_INTERVAL
 while true; do
   clear
