99bde2a169a192bb42718c4cf56173cba54cdf0c
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-11-25 12:25:05 +0530

    Add commands to integrate changes from remote.

--- startci.sh
+++ startci.sh
@@ -3,7 +3,7 @@
 repository_url=$1
 repository_name=$(echo "$repository_url" | cut -d"/" -f5 | cut -d"." -f1)
 CI_REPOS_DIR=".ci_repositories"
-INTEGRATION_INTERVAL=5
+INTEGRATION_INTERVAL=600
 
 #Create ".ci_repositories" directory if it does not already exists
 if [ ! -d "$CI_REPOS_DIR" ]; then
@@ -11,14 +11,16 @@ if [ ! -d "$CI_REPOS_DIR" ]; then
 fi
 cd "$CI_REPOS_DIR"
 
+#Clone repository if it is not already cloned
 if [ ! -d "$repository_name" ]; then
   git clone "$repository_url"
 fi
 cd "$repository_name"
 
+#Start running test after every INTEGRATION_INTERVAL
 while true; do
   clear
   mocha --reporter min
   sleep $INTEGRATION_INTERVAL
+  git pull > /dev/null
 done
-
